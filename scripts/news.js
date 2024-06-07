d294cb5809c4a3eba975d40d66406d077f396eda
소희 <105264974+sohee-commits@users.noreply.github.com>
2024-06-07 13:25:30 +0500

    filled news

--- scripts/news.js
+++ scripts/news.js
@@ -5,33 +5,39 @@ let newsList = [
 		name: `Вега.Логистика - О Нас`,
 		date: `04.06.2024`,
 		text: `
-    ООО "Вега" работает на рынке автомобильных перевозок с 2015 года.
-    Грузоперевозки – это ключевой элемент современной экономики, который обеспечивает эффективную и безопасную транспортировку грузов в различных условиях.  И выбор правильного перевозчика является решающим фактором для успешной доставки груза. Поэтому важно выбирать перевозчика, который имеет опыт работы в данной области и может предоставить гарантии на сохранность груза. <br />
-    Мы  успешно  прошли сложные моменты в сфере грузоперевозок, сохранив компанию и ее работоспособность. <br />
-    На данный момент  мы оказываем экспедиторские и логистическсие услуги по городу и России. <br />
-    Опытные перевозчики доставят ваш груз в кратчайшие сроки, сохранив вашу уверенность в целостности  и сохранности доверенного им груза. <br />
-    Наши перевозчики ценят доверие, оказанное вами, поэтому работают качественно, ответственно и надежно. <br />
-    Сопровождение сделки с момента поступления заявки до доставки груза в нужную точку.
-    Внимательное отношение к документообороту , быстрое и своевременное решение возникающих вопросов. <br />
+    Грузоперевозки – это ключевой элемент современной экономики, который обеспечивает эффективную и безопасную транспортировку грузов в различных условиях.  И выбор правильного перевозчика, имеющего  опыт работы в данной   области и  гарантии на сохранность груза,  является решающим фактором для успешной доставки груза. <br />
+		ООО «Вега» работает на рынке  автомобильных грузоперевозок с 2015 года, и наши перевозчики преданно  работают с нами. <br />
+		Мы  успешно  прошли сложные моменты в сфере грузоперевозок, сохранив компанию и ее работоспособность.  <br />
     `,
 	},
 	{
 		index: 1,
-		name: `Пример очень интересной новости в две строки`,
-		date: `29.05.2024`,
-		text: `Lorem Ipsum is a placeholder text widely used in the printing and typesetting industry. Originating in the 1500s, it has remained largely unchanged through the transition to electronic typesetting. Its primary purpose is to serve as a neutral fill text that mimics the appearance of real text without conveying actual meaning, allowing designers to focus on layout and typography without being distracted by content. This makes it ideal for use in design mock-ups, templates, and other situations where the visual presentation is under development but the final content is not yet available.`,
+		name: `Наши услуги и опыт`,
+		date: `27.06.2024`,
+		text: `
+		На данный момент  мы оказываем экспедиторские и логистические услуги по городу и России.  Для Вас работают эффективные, отзывчивые, четко ориентированные на  положительный результат менеджеры. Гибкая система скидок позволит Вам сэкономить на транспортных расходах. <br />
+		Опытные перевозчики доставят ваш груз в кратчайшие сроки, сохранив Вашу уверенность в целостности  и сохранности доверенного им груза. <br />
+		Наши перевозчики ценят доверие, оказанное Вами, поэтому работают  качественно, ответственно и надежно. <br />
+		`,
 	},
 	{
 		index: 2,
-		name: `Пример очень интересной новости в две строки`,
+		name: `Наша ответственность`,
 		date: `29.05.2024`,
-		text: `Lorem Ipsum is a placeholder text widely used in the printing and typesetting industry. Originating in the 1500s, it has remained largely unchanged through the transition to electronic typesetting. Its primary purpose is to serve as a neutral fill text that mimics the appearance of real text without conveying actual meaning, allowing designers to focus on layout and typography without being distracted by content. This makes it ideal for use in design mock-ups, templates, and other situations where the visual presentation is under development but the final content is not yet available.`,
+		text: `
+		Сопровождение сделки с момента поступления заявки до доставки груза в нужную точку.  <br />
+		Внимательное отношение к документообороту , быстрое и своевременное решение возникающих вопросов. <br />
+		Своевременная подача ТС по погрузку/выгрузку. <br />
+		`,
 	},
 	{
 		index: 3,
-		name: `Пример очень интересной новости в две строки`,
+		name: `Наши перевозчики`,
 		date: `29.05.2024`,
-		text: `Lorem Ipsum is a placeholder text widely used in the printing and typesetting industry. Originating in the 1500s, it has remained largely unchanged through the transition to electronic typesetting. Its primary purpose is to serve as a neutral fill text that mimics the appearance of real text without conveying actual meaning, allowing designers to focus on layout and typography without being distracted by content. This makes it ideal for use in design mock-ups, templates, and other situations where the visual presentation is under development but the final content is not yet available.`,
+		text: `
+		Уверенные в результате и ориентированные на успех, выполнят работу в кратчайшие сроки качественно.  <br />
+		Система контроля за движением груза (посредством email, sms, сайта, персонального менеджера) поможет оперативно следить за Вашим отправлением.
+		`,
 	},
 ];
 
