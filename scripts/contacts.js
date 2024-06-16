2de9c0f9376f261462a263c2dc6be986d4210428
소희 <105264974+sohee-commits@users.noreply.github.com>
2024-06-16 17:46:23 +0500

    copy logic change

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,24 +1,48 @@
-let copyBtnsList = document.querySelectorAll(`.copyBtn`);
+let copyBtnsList = document.querySelectorAll('.copyBtn');
 
 copyBtnsList.forEach((btn) => {
-	btn.addEventListener(`click`, () => {
+	btn.addEventListener('click', () => {
 		let content = btn.textContent;
-
-		if (!navigator.clipboard) {
-			alert(`Копирование текста не поддерживается в вашем браузере.`);
-			return;
+		// пробуем как обычно
+		if (navigator.clipboard) {
+			navigator.clipboard
+				.writeText(content)
+				.then(() => {
+					console.log(`Текст скопирован в буфер обмена:`, content);
+					alert(`Текст успешно скопирован (${content}).`);
+				})
+				.catch((err) => {
+					console.error(`Ошибка при копировании текста:`, err);
+					fallbackCopyTextToClipboard(content);
+				});
+		} else {
+			// не получается - копируем через execcomand
+			fallbackCopyTextToClipboard(content);
 		}
-
-		navigator.clipboard
-			.writeText(content)
-			.then(() => {
-				console.log(`Текст скопирован в буфер обмена:`, content);
-			})
-			.catch((err) => {
-				console.error(`Ошибка при копировании текста:`, err);
-				alert(
-					`Что-то пошло не так. Попробуйте заполнить форму заявки и мы свяжемся с Вами.`
-				);
-			});
 	});
 });
+
+function fallbackCopyTextToClipboard(text) {
+	// пробуем иначе
+	try {
+		let textarea = document.createElement(`textarea`);
+		textarea.value = text;
+		textarea.setAttribute(`readonly`, ``);
+		textarea.style.position = `absolute`;
+		textarea.style.left = `-9999px`;
+		document.body.appendChild(textarea);
+
+		textarea.select();
+		document.execCommand(`copy`);
+
+		document.body.removeChild(textarea);
+
+		console.log(`Текст скопирован в буфер обмена:`, text);
+		alert(`Текст успешно скопирован (${text}).`);
+	} catch (err) {
+		// выводим сообщение, если вообще никак не получается
+		alert(
+			`Что-то пошло не так. Попробуйте заполнить форму заявки и мы свяжемся с Вами.`
+		);
+	}
+}
