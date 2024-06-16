76117e85f352ef0bcf10cfd9fd01b788178480d6
소희 <105264974+sohee-commits@users.noreply.github.com>
2024-06-16 15:38:41 +0500

    for-partners page done

---
+++ for-partners.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Вега.Логистика - Партнерам</title>
+    <link rel="shortcut icon" href="./assets/logotype-icon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="./css/for-partners.css">
+  </head>
+
+  <body>
+    <?php require_once ('./header.php'); ?>
+
+    <main>
+      <section class="summ">
+        <div class="text">
+          <h2>Грузоперевозки</h2>
+          <p>
+            Грузоперевозки – это ключевой элемент современной экономики, который обеспечивает эффективную и безопасную
+            транспортировку грузов в различных условиях. Выбор правильного перевозчика, имеющего опыт работы в данной
+            области и предоставляющего гарантии на сохранность груза, является решающим фактором для успешной доставки
+            груза.
+          </p>
+        </div>
+        <img src="./assets/for-partners/1.png" alt="грузовая машина">
+      </section>
+
+      <section class="summ">
+        <img src="./assets/for-partners/2.png" alt="грузовая машина">
+        <div class="text">
+          <h2>О компании</h2>
+          <p>
+            ООО «Вега» работает на рынке автомобильных грузоперевозок с 2015 года, и наши перевозчики преданно работают
+            с нами. Мы успешно прошли сложные моменты в сфере грузоперевозок, сохранив компанию и ее работоспособность.
+          </p>
+        </div>
+      </section>
+
+      <div class="more">
+        <ul>
+          <li>
+            <h4>Логистические услуги</h4>
+            <p>Экспедирование и логистистические услуги по городу и России.</p>
+          </li>
+          <li>
+            <h4>Отзывчивые менеджеры</h4>
+            <p>Эффективные и ориентированные на положительный результат.</p>
+          </li>
+          <li>
+            <h4>Система скидок</h4>
+            <p>Гибкая система скидок для экономии на транспортных расходах.</p>
+          </li>
+        </ul>
+      </div>
+
+      <section class="responsibility">
+        <h2>Наша <span class="yellow">ответственность</span></h2>
+        <div class="info">
+          <ul>
+            <li>
+              <div class="list-style">
+                <div class="line first-line"></div>
+                <div class="circle"></div>
+                <div class="line"></div>
+              </div>
+              <p>Сопровождение сделки с момента поступления заявки до доставки груза в нужную точку</p>
+            </li>
+            <li>
+              <div class="list-style">
+                <div class="line"></div>
+                <div class="circle"></div>
+                <div class="line"></div>
+              </div>
+              <p>Внимательное отношение к документообороту, быстрое решение возникающих вопросов</p>
+            </li>
+            <li>
+              <div class="list-style">
+                <div class="line"></div>
+                <div class="circle"></div>
+                <div class="line last-line"></div>
+              </div>
+              <p>Своевременная подача транспортного средства для погрузки/выгрузки.</p>
+            </li>
+          </ul>
+          <img src="./assets/for-partners/3.png" alt="">
+        </div>
+        <ul class="pros">
+          <li class="pro">
+            Опытные перевозчики, доставляющие груз в кратчайшие сроки
+          </li>
+          <li class="pro">
+            Качественная, ответственная и надежная работа перевозчиков, ценящих доверие клиентов
+          </li>
+          <li class="pro">
+            Обеспечение целостности и сохранности доверенного груза
+          </li>
+        </ul>
+      </section>
+
+      <section class="cs">
+        <h2>Система <span class="yellow">контроля</span> за движением груза</h2>
+        <ul class="cs-list">
+          <li class="cs-item">
+            <div class="cs-img">
+              <img src="./assets/icons/icons8-letter-50.png" alt="иконка e-mail">
+            </div>
+            <p>E-MAIL</p>
+          </li>
+          <li class="cs-item">
+            <div class="cs-img">
+              <img src="./assets/icons/icons8-sms-50.png" alt="иконка sms">
+            </div>
+            <p>SMS</p>
+          </li>
+          <li class="cs-item">
+            <div class="cs-img">
+              <img src="./assets/icons/icons8-web-50.png" alt="иконка worldwide web">
+            </div>
+            <p>Веб-Сайт</p>
+          </li>
+          <li class="cs-item">
+            <div class="cs-img">
+              <img src="./assets/icons/icons8-manager-50.png" alt="иконка менеджера">
+            </div>
+            <p>Персональный Менеджер</p>
+          </li>
+        </ul>
+      </section>
+
+      <?php require_once './form.php' ?>
+
+    </main>
+
+    <?php require_once ('./footer.php'); ?>
+  </body>
+
+</html>
