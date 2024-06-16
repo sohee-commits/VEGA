96645cdbdc0393f9fb10910b3454defa6bbe1a04
소희 <105264974+sohee-commits@users.noreply.github.com>
2024-06-16 17:36:38 +0500

    alert(`Копирование текста не поддерживается в вашем браузере.`);

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -3,16 +3,22 @@ let copyBtnsList = document.querySelectorAll(`.copyBtn`);
 copyBtnsList.forEach((btn) => {
 	btn.addEventListener(`click`, () => {
 		let content = btn.textContent;
+
+		if (!navigator.clipboard) {
+			alert(`Копирование текста не поддерживается в вашем браузере.`);
+			return;
+		}
+
 		navigator.clipboard
 			.writeText(content)
 			.then(() => {
-				alert(`Текст скопирован: ${content}`);
+				console.log(`Текст скопирован в буфер обмена:`, content);
 			})
 			.catch((err) => {
+				console.error(`Ошибка при копировании текста:`, err);
 				alert(
 					`Что-то пошло не так. Попробуйте заполнить форму заявки и мы свяжемся с Вами.`
 				);
-				console.error(err);
 			});
 	});
 });
